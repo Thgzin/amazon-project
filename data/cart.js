2272c650e7641b940ea0705fb268880a59141c33
Thgzin <thgziin@outlook.com>
2025-05-26 19:00:06 -0300

    moveAddToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,38 @@
 export const cart = [];
+
+export function addToCart(productId) {
+  let matchingItem;
+
+  const selectProduct = document.querySelector(
+    `.select-quantity-${productId}`
+  ).value;
+
+  const selectValue = Number(selectProduct);
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  if (matchingItem) {
+    matchingItem.quantity = selectValue;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: selectValue,
+    });
+  }
+}
+
+export function updateCartQuantity() {
+  let cartQuantity = 0;
+
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+
+  document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+}
+
+
