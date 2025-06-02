fb22f4f19213fcbb7b4a910a6efa563e839b76c7
Thgzin <thgziin@outlook.com>
2025-06-02 17:13:12 -0300

    exercises part1

---
+++ exercises/exercises.js
@@ -0,0 +1,22 @@
+import dayjs from "https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js";
+
+/*
+function getDate() {
+  const date = dayjs();
+  //const dateAdd = date.add(5, "days");
+  //const dateAdd = date.add(1, "month");
+  //const dateAdd = date.subtract(1, "month");
+  //const dateAdd = date.subtract(1, "month");
+  //console.log(dateAdd.format("MMMM, D"));
+  //console.log(dateAdd.format("MMMM, D"));
+}
+
+
+function isWeekend(date) {
+  const dayOfWeek = date.format("dddd");
+  return dayOfWeek === "Saturday" || dayOfWeek === "Sunday";
+}
+let date = dayjs();
+date = date.add(6, "days");
+console.log(`${date.format("dddd")} | ${isWeekend(date)} `);
+*/
